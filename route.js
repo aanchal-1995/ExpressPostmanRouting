4b2650302913402fb95817793843fa313d8202de
aanchal-1995 <aanchalmalhotra1512@gmail.com>
2021-05-05 23:04:43 +0530

    Put delete request

--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const accounts = require('./database.js');
+var accounts = require('./database.js');
 route.get('/accounts', (req,res)=>{
     res.json({userData: accounts} );
 })
@@ -15,4 +15,30 @@ route.get('/accounts/:id',(req,res)=>{
     }
 } )
 
+route.put('/accounts/:id', (req,res)=>{
+    const accountid = Number(req.params.id);
+    const body = req.body;
+    const  account = accounts.find((account)=> account.id ===accountid);
+    const index = accounts.indexOf(account);
+    if(!account){
+        res.status(500).send("Account not found")
+    }else{
+        const updateAccount = {...account, ...body}
+        accounts[index] = updateAccount
+
+        res.send(updateAccount)
+    }
+})
+//delete request
+
+route.delete('/accounts/:id', (req,res)=>{
+    const accountid = Number(req.params.id);
+    const newaccounts = accounts.filter((account)=>(account.id !=accountid));
+    if(!newaccounts){
+        res.status(500).send("Account not found")
+    }else{
+        accounts = newaccounts
+        res.send(accounts)
+    }
+})
 module.exports = route
